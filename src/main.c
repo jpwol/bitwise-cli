163b11f9c5b67f206e7748160d5fdacf2bef1271
jpwol <jpwolfe1981@gmail.com>
2025-03-29 21:15:52 -0400

    added EOF escape from program

--- src/main.c
+++ src/main.c
@@ -1,14 +1,14 @@
 #include "Parse.h"
 
 int main() {
-  while (1) {
-    char input[MAX_EXPR_LEN];
+  printf("\033[35m>>>\033[0m ");
+  char input[MAX_EXPR_LEN];
 
+  while ((fgets(input, MAX_EXPR_LEN, stdin) != NULL)) {
     Token tokens[MAX_TOKENS];
     Token rpn[MAX_TOKENS];
 
-    printf("\033[35m>>>\033[0m ");
-    fgets(input, MAX_EXPR_LEN, stdin);
+    // printf("\033[35m>>>\033[0m ");
     // newlines in my buffer? more likely than you think
     input[strcspn(input, "\n")] = '\0';
 
@@ -16,6 +16,7 @@ int main() {
     int rpn_count = shunting_yard(tokens, token_count, rpn);
 
     printf("%d\n", evaluate(rpn, rpn_count));
+    printf("\033[35m>>>\033[0m ");
   }
 
   return 0;
